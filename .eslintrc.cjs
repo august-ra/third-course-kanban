5fdd55211f4c6b874872fd6078a833e8fec624f8
Anton Voytov <august-ra@ya.ru>
2024-05-09 13:39:57 +0300

    Configured ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [ 'dist', '.eslintrc.cjs' ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: [
@@ -15,12 +15,28 @@ module.exports = {
     'react-refresh',
   ],
   rules: {
-    '@stylistic/js/semi': [ "error", "never", { "beforeStatementContinuationChars": "never" } ],
+    '@stylistic/js/arrow-parens': [ 'error', 'always' ],
+    '@stylistic/js/arrow-spacing': [ 'error', { 'before': true, 'after': true } ],
+    '@stylistic/js/comma-dangle': [ 'error', 'always-multiline' ],
+    '@stylistic/js/function-call-spacing': [ 'error', 'never' ],
+    '@stylistic/js/indent': [ 'error', 2 ],
+    '@stylistic/js/key-spacing': [ 'error', { 'align': 'value' } ],
+    '@stylistic/js/no-trailing-spaces': 'error',
+    '@stylistic/js/object-curly-spacing': [ 'error', 'always' ],
+    '@stylistic/js/operator-linebreak': [ 'error', 'before', {
+      'overrides': {
+        '=': 'ignore',
+        '+=': 'ignore',
+        '-=': 'ignore',
+        '*=': 'ignore',
+        '/=': 'ignore',
+        '%=': 'ignore',
+      }
+    } ],
+    '@stylistic/js/rest-spread-spacing': [ 'error', 'never' ],
+    '@stylistic/js/semi': [ 'error', 'never', { 'beforeStatementContinuationChars': 'never' } ],
     'react/jsx-no-target-blank': 'off',
     'react/prop-types': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true }
-    ],
+    'react-refresh/only-export-components': [ 'warn', { 'allowConstantExport': true } ],
   },
 }
