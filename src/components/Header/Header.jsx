81f29876c4be402b233374587681b99cc5192f46
Anton Voytov <august-ra@ya.ru>
2024-05-18 10:44:09 +0300

    Made hook isOpened and handled it to a link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,24 @@
-import React from "react"
+import { useState } from "react"
 import PopUser from "../Popups/PopUser/PopUser"
 
 
 const Header = () => {
+  const [isOpened, setIsOpened] = useState(false)
+
+  const handleOpen = () => {
+    console.log(isOpened)
+    setIsOpened((prev) => !prev)
+  }
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__block">
           <div className="header__logo _show _light">
-            <a href="" target="_self"><img src="images/logo.png" alt="logo" /></a>
+            <a href="" target="_self"><img src="/images/logo.png" alt="logo" /></a>
           </div>
           <div className="header__logo _dark">
-            <a href="" target="_self"><img src="images/logo_dark.png" alt="logo" /></a>
+            <a href="" target="_self"><img src="/images/logo_dark.png" alt="logo" /></a>
           </div>
 
           <nav className="header__nav">
@@ -19,9 +26,9 @@ const Header = () => {
               <a href="#popNewCard">Создать новую задачу</a>
             </button>
 
-            <a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
+            <a href="#user-set-target" className="header__user _hover02" onClick={handleOpen}>Ivan Ivanov</a>
 
-            <PopUser />
+            { isOpened && <PopUser /> }
           </nav>
         </div>
       </div>
