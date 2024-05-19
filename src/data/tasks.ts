b01956f91da6e895731e9a174aaf207deb383244
Anton Voytov <august-ra@ya.ru>
2024-05-19 13:48:18 +0300

    Made statuses.ts and tasks.ts

---
+++ src/data/tasks.ts
@@ -0,0 +1,46 @@
+
+export interface TaskData {
+  id:     number
+  theme:  string
+  title:  string
+  date:   string
+  status: string
+}
+
+export const Tasks: TaskData[] = [
+  {
+    id:     1,
+    theme:  "Web Design",
+    title:  "Task 1",
+    date:   "18.05.24",
+    status: "Без статуса",
+  },
+  {
+    id:     2,
+    theme:  "Research",
+    title:  "Task 2",
+    date:   "14.05.24",
+    status: "Нужно сделать",
+  },
+  {
+    id:     3,
+    theme:  "Copywriting",
+    title:  "Task 3",
+    date:   "10.05.24",
+    status: "В работе",
+  },
+  {
+    id:     4,
+    theme:  "Research",
+    title:  "Task 4",
+    date:   "06.05.24",
+    status: "Тестирование",
+  },
+  {
+    id:     5,
+    theme:  "Web Design",
+    title:  "Task 5",
+    date:   "02.05.24",
+    status: "Готово",
+  },
+]
