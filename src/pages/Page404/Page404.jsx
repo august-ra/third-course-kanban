dab4003886ccea5fcd71b426af38238ef4dba0e1
Anton Voytov <august-ra@ya.ru>
2024-06-08 00:50:28 +0300

    Hotfix

--- src/pages/Page404/Page404.jsx
+++ src/pages/Page404/Page404.jsx
@@ -7,11 +7,11 @@ import * as Styled from "./Page404.styled"
 function Page404() {
   return (
     <Styled.Page404Container>
-      <img src="./images/404.png" />
+      <img src="./images/404.png" alt="404" />
       <Styled.Page404TextBlock>
         <Styled.Page404Title>Страница не найдена</Styled.Page404Title>
         <Styled.Page404Description>
-          Страница, на которую Вы перешли, не найдена. Возможно, вы ввели неправильный адрес, или страница была перемещена.
+          Страница, на которую Вы перешли, не найдена. Возможно, Вы ввели неправильный адрес, или страница была перемещена.
         </Styled.Page404Description>
         <Link to={Pages.MAIN}>Перейти на главную</Link>
       </Styled.Page404TextBlock>
